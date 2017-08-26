f742ab507a80b554fcaab1dedaa705b0734f8d09
limin <min.a.li@g-emall.com>
2017-08-26 18:08:29 +0800

    登录的简单实现

--- backend/models/UserBackend.php
+++ backend/models/UserBackend.php
@@ -76,6 +76,17 @@ class UserBackend extends \yii\db\ActiveRecord implements IdentityInterface
     }
 
     /**
+     * Finds user by username
+     *
+     * @param string $username
+     * @return static|null
+     */
+    public static function findByUsername($username)
+    {
+        return static::findOne(['username' => $username]);
+    }
+
+    /**
      * @inheritdoc
      * 用以标识 Yii::$app->user->id 的返回值
      */
@@ -110,6 +121,17 @@ class UserBackend extends \yii\db\ActiveRecord implements IdentityInterface
     public function setPassword($password)
     {
         $this->password_hash = Yii::$app->security->generatePasswordHash($password);
+    }
+
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return boolean if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return Yii::$app->security->validatePassword($password, $this->password_hash);
     }
 
     /**
